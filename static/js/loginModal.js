f70b45e0439fafebad939ef1fcb5464d256e46c2
S-troup10 <simontroup27@gmail.com>
2025-06-22 23:29:18 +1000

    Update loginModal.js

--- static/js/loginModal.js
+++ static/js/loginModal.js
@@ -82,7 +82,7 @@ function AttemptLogin() {
 }
 
 function Login(email, password) {
-    fetch("http://127.0.0.1:5000/login", {
+    fetch("/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: email, password: password }),
